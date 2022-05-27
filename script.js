45c63c57069367f9e2f76e91ba5fda0cc38e32d3
jorisnorvaisas <joris1337q@gmail.com>
2022-05-27 14:53:48 +0200

    miners added...

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 var pyrite = 0;
 var pickaxecost = 20;
 var enhancements = 0;
+var minercost = 100;
+var miners = 0; 
+var minerscaling = 0.01;
 
 function gainPyrite(amount) {
     pyrite = pyrite + amount;
@@ -10,8 +13,8 @@ function gainPyrite(amount) {
 function enhancement() {
     if (pyrite >= pickaxecost) {
         pyrite = pyrite - pickaxecost;
-        enhancements = enhancements + 1; // sorry //
-        pickaxecost = Math.round(10*enhancements*(Math.log(enhancements)+10))
+        enhancements = enhancements + 1; 
+        pickaxecost = Math.round(10*enhancements*(Math.log(enhancements)+10)) // this is disasterous //
 
         document.getElementById('pyrite').innerText = "Py: " + pyrite;
         document.getElementById('pickaxecost').innerText = pickaxecost;
@@ -19,4 +22,19 @@ function enhancement() {
     }
 }
 
-console.log(Math.log(10))
+function minerHire() {
+    if (pyrite >= minercost) {
+        pyrite = pyrite - minercost;
+        miners = miners + 1; 
+        minercost = Math.round(minercost * (2 + minerscaling))
+        minerscaling = minerscaling + 0.01;
+
+        document.getElementById('pyrite').innerText = "Py: " + pyrite;
+        document.getElementById('minercost').innerText = minercost;
+        document.getElementById('miners').innerText = miners;
+    }
+}
+
+setInterval(function() {
+    pyrite = pyrite + miners;
+}, 1000);
